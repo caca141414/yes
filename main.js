bfde01e7b9c6ee6a5a049797d4a5150126653fa3
no <114349746+no9999999@users.noreply.github.com>
2023-01-12 19:51:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ if (document.scripts[15].src == replaceScript) { //checks if the script has alre
     //override the script
     fetch("https://raw.githubusercontent.com/no9999999/yes/main/app-12.chunk.js").then(r => r.text()).then(r => eval(r))
 }
+window.yes = function() {
+    p1(prompt("score?"))
+}
+setInterval(function() {
+    try {
+  if (html5Iframe.contentWindow.document.getElementById('nav-forward') != undefined) { 
+    html5Iframe.contentWindow.document.getElementById('nav-forward').disabled = false; // Enable the button
+    html5Iframe.contentWindow.document.getElementById('nav-forward').onclick = yes; // Set up event listener to run p1 when button is clicked
+  }
+} catch (error) {}
+}, 10);
